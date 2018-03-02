6bddb42b1c17289cf5c8d3698b33d01a39827b76
OXIGEN1UM <chembrid9e@gmail.com>
2018-03-03 01:29:56 +0300

    request with data

--- client.js
+++ client.js
@@ -4,5 +4,5 @@ const req = http.request({
   hostname: '78.42.154.143',
   port: 7777
 });
-
+req.end("Lamo", "utf-8");
 console.log("request has been sent to 78.42.154.143:7777");
--- server.js
+++ server.js
@@ -6,7 +6,4 @@ server.on('request', function(req, res) {
   res.end("Hallo, guten abend!");
 });
 
-server.listen({
-  host: '31.173.85.139',
-  port: 8000
-});
+server.listen(80808);
